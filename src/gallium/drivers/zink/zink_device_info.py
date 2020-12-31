e992bcd7dcc69ca2b9b08cb6ef8998a8d5e99c32
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-12-31 09:21:39 -0500

    zink: hook up VK_EXT_descriptor_indexing

--- src/gallium/drivers/zink/zink_device_info.py
+++ src/gallium/drivers/zink/zink_device_info.py
@@ -173,6 +173,11 @@ EXTENSIONS = [
         alias="template"),
     Extension("VK_KHR_dedicated_allocation",
         alias="dedicated"),
+    Extension("VK_EXT_descriptor_indexing",
+        alias="desc_indexing",
+        features=True,
+        properties=True,
+        conditions=["$feats.descriptorBindingPartiallyBound"]),
 ]
 
 # constructor: Versions(device_version(major, minor, patch), struct_version(major, minor))
