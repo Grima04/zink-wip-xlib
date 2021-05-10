8ccb5dca64469e694fe9e795e7b2cd40a455cd44
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-10 17:34:25 -0400

    zink: make shader cache local to a single program
    sharing caches makes things complicated and doesn't yield much benefit

--- src/gallium/drivers/zink/zink_program.h
+++ src/gallium/drivers/zink/zink_program.h
@@ -68,12 +68,6 @@ struct zink_shader_module {
    VkShaderModule shader;
 };
 
-/* the shader cache stores a mapping of zink_shader_key::VkShaderModule */
-struct zink_shader_cache {
-   struct pipe_reference reference;
-   struct hash_table *shader_cache;
-};
-
 struct zink_program {
    struct pipe_reference reference;
    unsigned char sha1[20];
@@ -88,6 +82,9 @@ struct zink_program {
    VkPipelineLayout layout;
    VkDescriptorSetLayout dsl[ZINK_DESCRIPTOR_TYPES + 1]; // one for each type + push
    unsigned num_dsl;
+
+   /* the shader cache stores a mapping of zink_shader_key::VkShaderModule */
+   struct hash_table shader_cache;
 };
 
 struct zink_gfx_program {
@@ -100,7 +97,6 @@ struct zink_gfx_program {
    struct zink_shader *last_vertex_stage;
 
    struct zink_shader *shaders[ZINK_SHADER_COUNT];
-   struct zink_shader_cache *shader_cache;
    unsigned char shader_slot_map[VARYING_SLOT_MAX];
    unsigned char shader_slots_reserved;
    struct hash_table *pipelines[11]; // number of draw modes we support
@@ -111,7 +107,6 @@ struct zink_compute_program {
 
    struct zink_shader_module *module;
    struct zink_shader *shader;
-   struct zink_shader_cache *shader_cache;
    struct hash_table *pipelines;
 };
 
