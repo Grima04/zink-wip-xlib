37fc1f69e3fed557d0148a956309d99675647db8
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-12 08:15:25 -0400

    zink: move mem cache to sub-struct
    no functional changes

--- src/gallium/drivers/zink/zink_screen.h
+++ src/gallium/drivers/zink/zink_screen.h
@@ -101,10 +101,12 @@ struct zink_screen {
 
    struct util_live_shader_cache shaders;
 
-   simple_mtx_t mem_cache_mtx;
-   struct hash_table *resource_mem_cache;
-   uint64_t mem_cache_size;
-   unsigned mem_cache_count;
+   struct {
+      simple_mtx_t mem_cache_mtx;
+      struct hash_table *resource_mem_cache;
+      uint64_t mem_cache_size;
+      unsigned mem_cache_count;
+   } mem;
 
    unsigned shader_id;
 
